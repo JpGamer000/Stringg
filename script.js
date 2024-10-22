2f373972f57e71182efd05612be62a0261d7f1e7
JpGamer000 <163525423+JpGamer000@users.noreply.github.com>
2024-10-22 12:09:46 -0300

    sklaaaa

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 function somaStrings(string){
 let contador = 0;
 for (var i =0; i<string.length; i++){
-    if  (string[i] !== ' '){
+    if  (string[i] !== " "){
         contador++;
     }
 }
-return contador;
+return contador
+}
 
+
+function palindromo(string){
+    let contador = 0;
+    for (var i=0; i<string.length; i++){
+        if (string[i] !== " "){
+            string += string[i].toLowerCase()
+            contador++            
+
+        }
+    }
+    let comeco = 0;
+    let final = string.length - 1;
+    for (comeco < final) {
+        if (string[comeco] != string[final]){
+            return false;
+        }
+            comeco++;
+            final--;
+    }
+    return true;
 }
